d698c448d5a511d155248ebf01f7ccc9c08596ab
okynos <pylott@gmail.com>
2018-07-12 14:41:46 +0200

    Fixing params display in inventory

--- jump/utils/JSON2Inventory.py
+++ jump/utils/JSON2Inventory.py
@@ -1,31 +1,38 @@
 import json
 import sys
 
+def printParams(pathToKey):
+  print("ansible_user=vagrant")
+  print("ansible_ssh_private_key_file=" + pathToKey)
+  print("ansible_ssh_common_args='-o StrictHostKeyChecking=no'")
+
 with open(sys.argv[1]) as json_data:
-    data = json.load(json_data)
-    agents = ""
-    managers = ""
+  data = json.load(json_data)
+  agents = ""
+  managers = ""
 
-    for element in data:
-      hostname = element['hostname']
-      ip = element['IP']
-      identifier = str(element['ID'])
+  for element in data:
+    hostname = element['hostname']
+    ip = element['IP']
+    identifier = str(element['ID'])
 
-      if hostname == 'Agent':
-        #print('[Agents]')
-        agents = agents + hostname + identifier + ' ansible_host=' + ip
-        agents += '\n'
-      else:
-        managers = managers + hostname + identifier + ' ansible_host=' + ip
+    if hostname == 'Agent':
+      #print('[Agents]')
+      agents = agents + hostname + identifier + ' ansible_host=' + ip
+      agents += '\n'
+    else:
+      managers = managers + hostname + identifier + ' ansible_host=' + ip
 
-    print("[Agents]")
-    print(agents)
-    print("[Managers]")
-    print(managers)
-    print("\n")
+  print("[Agents]")
+  print(agents)
+  print("[Managers]")
+  print(managers)
+  print("")
 
-    print("[Agents:vars]")
-    print("ansible_user=vagrant ansible_ssh_private_key_file=" + sys.argv[2] + " ansible_ssh_common_args='-o StrictHostKeyChecking=no'")
-    print("\n")
-    print("[Managers:vars]")
-    print("ansible_user=vagrant ansible_ssh_private_key_file=" + sys.argv[2] + " ansible_ssh_common_args='-o StrictHostKeyChecking=no'")
+  print("[Agents:vars]")
+  printParams(sys.argv[2])
+  print("")
+  print("[Managers:vars]")
+  printParams(sys.argv[2])
+
+
